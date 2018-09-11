8dbb4c8d8fc883c30327be36baac4cf8f39ce906
Auclair Emmanuel <auclair.emmanuel@gmail.com>
2018-09-11 22:31:01 +0200

    Save certificates in local keystore at startup

--- src/mon_compte_connector/certs.clj
+++ src/mon_compte_connector/certs.clj
@@ -28,22 +28,22 @@
               (nil? root-ca) (throw-error "root-ca certificate not found")
               (nil? amaris-ca) (throw-error "amaris-ca certificate not found")
               :else (let [client-ks (doto (KeyStore/getInstance "PKCS12")
-                                     (.load nil nil)
-                                     (.setKeyEntry "client-cert" private-key
-                                                   (.toCharArray private-pwd) client-cert)
-                                     (.setCertificateEntry "root-ca" root-ca)
-                                     (.setCertificateEntry "amaris-ca" amaris-ca))
-                         server-ks (doto (KeyStore/getInstance "PKCS12")
-                                     (.load nil nil)
-                                     (.setKeyEntry "server-cert" private-key
-                                                   (.toCharArray private-pwd) server-cert)
-                                     (.setCertificateEntry "root-ca" root-ca)
-                                     (.setCertificateEntry "amaris-ca" amaris-ca))]
-                     {:client {:keystore client-ks
-                               :keystore-pass private-pwd
-                               :trust-store client-ks}
-                      :server {:keystore server-ks
-                               :keystore-pass private-pwd}})))))
+                                      (.load nil nil)
+                                      (.setKeyEntry "client-cert" private-key
+                                                    (.toCharArray private-pwd) client-cert)
+                                      (.setCertificateEntry "root-ca" root-ca)
+                                      (.setCertificateEntry "amaris-ca" amaris-ca))
+                          server-ks (doto (KeyStore/getInstance "PKCS12")
+                                      (.load nil nil)
+                                      (.setKeyEntry "server-cert" private-key
+                                                    (.toCharArray private-pwd) server-cert)
+                                      (.setCertificateEntry "root-ca" root-ca)
+                                      (.setCertificateEntry "amaris-ca" amaris-ca))]
+                      {:client {:keystore client-ks
+                                :keystore-pass private-pwd
+                                :trust-store client-ks}
+                       :server {:keystore server-ks
+                                :keystore-pass private-pwd}})))))
       (catch Exception error
         (println error)
         (throw-error (.getMessage error))))))
@@ -54,5 +54,28 @@
    :certs-file-pwd private-pwd})
 
 
-(defmethod ig/init-key :certs [_ config]
-  (load (merge default-certs config)))
+(defn store
+  [{{client-ks :keystore} :client
+    {server-ks :keystore} :server}]
+  (let [private-key (.getKey server-ks "server-cert" (.toCharArray private-pwd))
+        client-cert (.getCertificateChain client-ks "client-cert")
+        server-cert (.getCertificateChain server-ks "server-cert")
+        root-ca (.getCertificate server-ks "root-ca")
+        amaris-ca (.getCertificate server-ks "amaris-ca")]
+    (with-open [os (io/output-stream (:certs-file-path default-certs))]
+      (doto (KeyStore/getInstance "PKCS12")
+        (.load nil nil)
+        (.setKeyEntry "client-cert" private-key
+                      (.toCharArray private-pwd) client-cert)
+        (.setKeyEntry "server-cert" private-key
+                      (.toCharArray private-pwd) server-cert)
+        (.setCertificateEntry "root-ca" root-ca)
+        (.setCertificateEntry "amaris-ca" amaris-ca)
+        (.store os (.toCharArray private-pwd))))))
+
+
+(defmethod ig/init-key :certs [_ {:keys [certs-file-path certs-file-pwd] :as config}]
+  (let [new-config? (and certs-file-path certs-file-pwd)
+        certs (load (if new-config? config default-certs))]
+    (when new-config? (store certs))
+    certs))
